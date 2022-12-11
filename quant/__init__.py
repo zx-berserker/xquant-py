2c6b9424a81f3556fc40e538dabc7a33381a5e18
zhouxu <zhouxufunny@sina.com>
2022-12-11 20:29:13 +0800

    conda

--- quant/__init__.py
+++ quant/__init__.py
@@ -3,3 +3,10 @@
 date: 2022/12/1
 author: Berserker
 """
+
+
+from quant.tool.database.base import SQLAlchemy
+
+
+def quant_database_init():
+    SQLAlchemy.create_all()
---
+++ xquant.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*-
+"""
+date: 2022/12/1
+author: Berserker
+"""
+
+from quant import quant_database_init
+
+if __name__ == '__main__':
+    quant_database_init()
